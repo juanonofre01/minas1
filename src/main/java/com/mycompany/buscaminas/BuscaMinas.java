3ea51b3a49fd782a7741608155e5ee6500fd769b
DESKTOP-IB90627\PC <juana.achicanoy221@umariana.edu.co>
2023-04-28 01:00:47 -0500

    terminado interfaz

---
+++ src/main/java/com/mycompany/buscaminas/BuscaMinas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.buscaminas;
+
+/**
+ *
+ * @author PC
+ */
+public class BuscaMinas {
+
+    public static void main(String[] args) {
+        
+        Login v = new Login();
+        v.setVisible(true);
+    }
+}
